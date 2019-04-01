18531f40533ecec54b7ce43568fdcc2bd4cba7d8
manksharma <2mayanksharma2@gmail.com>
2019-03-31 23:06:43 -0700

    i think initial commit

---
+++ ExcellentTipCalculator/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  ExcellentTipCalculator
+//
+//  Created by Mayank Sharma on 3/31/19.
+//  Copyright © 2019 Mayank Sharma. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ExcellentTipCalculator/ViewController.swift
+++ ExcellentTipCalculator/ViewController.swift
@@ -7,14 +7,88 @@
 //
 
 import UIKit
+        
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var doneToolbar: UIToolbar!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipRateDefaults: UISegmentedControl!
+    @IBOutlet weak var tipRateField: UITextField!
+    @IBOutlet weak var tipAmntLabel: UILabel!
+    @IBOutlet weak var totalAmntLabel: UILabel!
+    
+    //var fieldsList = [billField, tipRateField]
+    
+    var tipRate: Double = 0.1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        billField.inputAccessoryView = doneToolbar
+        tipRateField.inputAccessoryView = doneToolbar
+        
+    }
+    
+    @IBAction func doneButton(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func donebuttonAction() {
+        self.view.endEditing(true)
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+        
+        // calculateTip(sender)
 
+    }
+    
+    @IBAction func calculateTip(_ sender: Any){
+        // Get bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        // Calculate tip
+        let tipAmnt = tipRate*bill
+        
+        // Calculate total
+        let total = tipAmnt + bill
+        
+        // Display results
+        tipAmntLabel.text = String(format: "$%.2f", tipAmnt)
+        totalAmntLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    @IBAction func onBillAmntEditChange(_ sender: Any) {
+        
+        calculateTip(sender)
+    }
+    
+    
+    @IBAction func onTipRateEditChange(_ sender: Any) {
+        
+        tipRate = Double(tipRateField.text!) ?? 10
+        tipRate = tipRate/100
+        calculateTip(sender)
+    }
+    
+    @IBAction func onDefaultValueChange(_ sender: Any) {
+        
+        var defaults = [0.1, 0.15, 0.2, 0.25]
+        
+        tipRate = defaults[tipRateDefaults.selectedSegmentIndex]
+        
+        print(tipRateDefaults.selectedSegmentIndex)
+        
+        calculateTip(sender)
+    }
+    
+    
 }
 
